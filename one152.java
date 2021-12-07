57d5c69fb57afd6ec4be03698bfca609161b8f0c
whiteuserface <white.user.face@gmail.com>
2021-12-07 20:37:34 +0900

    2021.12.07Final

--- one152.java
+++ one152.java
@@ -5,24 +5,41 @@ import java.io.IOException;
 public class one152 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		
+		
+		
+		int cnt=0;
+		int len=0;
+		
 		String inputString = br.readLine();
-		int cnt = 1;
+		len = inputString.length();
+//		System.out.println(len);
 		
-		for(int i=0; i<inputString.length(); i++) {
-			
-			if(inputString.charAt(i) == ' ') {
-				if(inputString.charAt(i)==inputString.charAt(i+1)) {
-					cnt++;
-				} else {
-					cnt++;
-				}
+		char[] inputArray = new char[inputString.length()];
+//		System.out.println(inputArray.length);
+		
+		for(int i=0; i<inputArray.length; i++) {
+			inputArray[i] = inputString.charAt(i);
+		}
+		
+		for(int i=0; i<inputArray.length; i++) {
+			if(i == inputArray.length-1) {
+				cnt++;
+				break;
 			}
+			if((inputArray[i]==' ')&&(inputArray[i+1]==' ')) {
+				continue;
+			} else if ((inputArray[i]==' ')&&(inputArray[i+1]!=' ')){
+				cnt++;
+			} 
 		}
-		if(inputString.charAt(0)==' ') {
-			cnt = cnt-1;
+		if(inputArray[0]==' ') {
+			cnt-=1;
 		}
-		
+//		if(inputArray[inputArray.length-1]==' ') {
+//			cnt-=1;
+//		}
 		System.out.println(cnt);
+		
 	}
 }
