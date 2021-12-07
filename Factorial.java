d610371d4bfcb86e2cc7dad3ea33b9d074fa6435
whiteuserface <white.user.face@gmail.com>
2021-12-07 15:06:28 +0900

    second

--- Factorial.java
+++ Factorial.java
@@ -3,20 +3,14 @@ import java.util.Scanner;
 public class Factorial {	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		 boolean oc = true;
-		while(oc) {
-			int a = sc.nextInt();
-			String b = sc.next();
-			char[] arr = new char[a];
-			char[] fullArr = new char[a*b.length()];
-			for(int i =0; i<a*b.length(); i=i+a) {
-				if(i>0&&i<=3) {
-					i-=1;	
-				}
-				arr[i/a] =b.charAt(i); 
-				System.out.print(arr[i/a]);
-			}
-			
+		int n = sc.nextInt();
+		System.out.println(factorial(n));
+	}
+	public static int factorial(int n) {
+		if(n == 1) {
+			return 1;
+		} else {
+			return n*factorial(n-1);
 		}
 	}
 }
--- a8958.java
+++ a8958.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class a8958 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		System.out.print("¹è¿­ °¹¼ö:");
 		int input = (int)br.read();
 		input = (int)(input-48);
